44eb038cb921ad7dd381b0f82f4fd6b6a55c4b68
SouoWill <137972464+SouoWill@users.noreply.github.com>
2023-10-04 00:30:22 -0300

    .

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+const menuDiv = document.getElementById('itens')
+const menuAnimar = document.getElementById('burguer')
+
+menuDiv.addEventListener('click', clickMenu)
+function clickMenu(){
+    menuDiv.classList.toggle('abrir-menu')
+    menuAnimar.classList.toggle('ativo')
+    const btn = document.getElementById('burguer')
+    btn.classList.toggle('animacao-menu')
+}
